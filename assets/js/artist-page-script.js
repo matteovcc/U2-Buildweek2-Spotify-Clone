d495ba9fa5e80430b8c34cc296fa06eef93c8ccc
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-03-24 16:06:14 +0100

    +

--- assets/js/artist-page-script.js
+++ assets/js/artist-page-script.js
@@ -1,25 +1,27 @@
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get("id")
-const endpoint = id ? "https://striveschool-api.herokuapp.com/api/deezer/artist/" + id : "https://striveschool-api.herokuapp.com/api/deezer/artist/412"
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const endpoint = id
+  ? "https://striveschool-api.herokuapp.com/api/deezer/artist/" + id
+  : "https://striveschool-api.herokuapp.com/api/deezer/artist/412";
 
 fetch(endpoint)
-.then(response => response.json())
-.then(artistData => {
-    displayArtist(artistData)
-    artistPopularSongs(artistData)
-    displayArtistAlbum(artistData)
-    console.log(artistData)
-})
-.catch((error) => {
-    console.error(error)
-})
+  .then(response => response.json())
+  .then(artistData => {
+    displayArtist(artistData);
+    artistPopularSongs(artistData);
+    displayArtistAlbum(artistData);
+    console.log(artistData);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 function displayArtist(artistData) {
-    const artistCover = document.getElementById("artistPage")
-    artistCover.style.backgroundImage = "url('"+ artistData.picture_xl +"')"
-    artistCover.style.backgroundPosition = "top"
-    artistCover.style.backgroundSize = "cover"
-    artistPage.innerHTML += `<div class="container-fluid mt-5 ms-2">
+  const artistCover = document.getElementById("artistPage");
+  artistCover.style.backgroundImage = "url('" + artistData.picture_xl + "')";
+  artistCover.style.backgroundPosition = "top";
+  artistCover.style.backgroundSize = "cover";
+  artistPage.innerHTML += `<div class="container-fluid mt-5 ms-2">
     <div class="row g-1">
       <div class="col-6">
         <div class="d-flex">
@@ -31,27 +33,27 @@ function displayArtist(artistData) {
       </div>
           
     </div> 
-  </div>`
+  </div>`;
 }
 
-function artistPopularSongs(artistData){
-    fetch(endpoint + "/top?limit=50")
-    .then((response) => response.json())
-    .then((popularSongs) => {
-        console.log(popularSongs)
-        const songsContainer = document.getElementById("popularArtistSongs")
-        const popularSongsList = document.createElement("ol")
-        songsContainer.appendChild(popularSongsList)
-        let index = 1
-        popularSongs.data.forEach(song => {
-          const minutes = Math.floor(song.duration / 60);
-          const seconds = Math.floor(song.duration % 60);
-          const durationString = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-            const songArtist = document.createElement("li")
-            songArtist.style.display = "flex"
-            songArtist.style.justifyContent = "space-between"
-            songArtist.style.alignItems = "center"
-            songArtist.innerHTML = `<div class="d-flex align-items-center gap-3">
+function artistPopularSongs(artistData) {
+  fetch(endpoint + "/top?limit=50")
+    .then(response => response.json())
+    .then(popularSongs => {
+      console.log(popularSongs);
+      const songsContainer = document.getElementById("popularArtistSongs");
+      const popularSongsList = document.createElement("ol");
+      songsContainer.appendChild(popularSongsList);
+      let index = 1;
+      popularSongs.data.forEach(song => {
+        const minutes = Math.floor(song.duration / 60);
+        const seconds = Math.floor(song.duration % 60);
+        const durationString = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+        const songArtist = document.createElement("li");
+        songArtist.style.display = "flex";
+        songArtist.style.justifyContent = "space-between";
+        songArtist.style.alignItems = "center";
+        songArtist.innerHTML = `<div class="d-flex align-items-center gap-3">
             <span class="text-light">${index}</span> 
             <a href="album-page.html?id=${song.album.id}"><img src="${song.album.cover_small}" alt="artistTrack" width="35" height="35"></a>
             <p class=text-light fw-bold >${song.title}</p> 
@@ -59,58 +61,57 @@ function artistPopularSongs(artistData){
            <div class="d-flex gap-5">
              <p class=text-light>${song.rank}</p>
              <p class=text-light>${durationString}</p>
-           </div>`
-           popularSongsList.appendChild(songArtist)
-           index++
+           </div>`;
+        popularSongsList.appendChild(songArtist);
+        index++;
 
-           songArtist.addEventListener("click", () => {
-            playTrack(song.preview);
-            const playerImg = document.getElementById("song-img");
-            const songSinger = document.getElementById("song-artist");
-            const songTitle = document.getElementById("song-title");
+        songArtist.addEventListener("click", () => {
+          playTrack(song.preview);
+          const playerImg = document.getElementById("song-img");
+          const songSinger = document.getElementById("song-artist");
+          const songTitle = document.getElementById("song-title");
 
+          playerImg.src = song.album.cover_small;
+          songTitle.innerText = song.title;
+          songSinger.innerText = song.artist.name;
+        });
 
-            playerImg.src = song.album.cover_small;
-            songTitle.innerText = song.title;
-            songSinger.innerText = song.artist.name;
-           })
-
-           const playBtn = document.getElementById("play");
-           playBtn.addEventListener("click", () => {
-            playTrack()
-            playBtn.innerHTML = `<span class="text-center text-dark fw-bold d-flex rounded-circle">| |</span>`
-           })
-           function playTrack(trackUrl) {
-            const audioPlayer = document.getElementById("audio-player")
-            audioPlayer.src = trackUrl
-            audioPlayer.play();
-           }
-        })
-        const favoriteSongs = document.getElementById("favoriteArtistSongs")
-        const info = document.createElement("div")
-        info.style.display = "flex"
-        info.style.justifyContent = "start"
-        info.style.alignItems = "center"
-        info.innerHTML = `<a href="artist-page.html?id=${artistData.id}"><img src=${artistData.picture_small} class="rounded-circle me-2" width="60" height="60"></a>
-        <p class="text-light"><span class="text-light fw-bold">Hai messo Mi piace a 11 brani</span><br>di <a href="artist-page.html?id=${artistData.id}">${artistData.name}</a></p>`
-        favoriteSongs.appendChild(info)
-    })
+        const playBtn = document.getElementById("play");
+        playBtn.addEventListener("click", () => {
+          playTrack();
+          playBtn.innerHTML = `<span class="text-center text-dark fw-bold d-flex rounded-circle"><i class="bi bi-pause"></i></span>`;
+        });
+        function playTrack(trackUrl) {
+          const audioPlayer = document.getElementById("audio-player");
+          audioPlayer.src = trackUrl;
+          audioPlayer.play();
+        }
+      });
+      const favoriteSongs = document.getElementById("favoriteArtistSongs");
+      const info = document.createElement("div");
+      info.style.display = "flex";
+      info.style.justifyContent = "start";
+      info.style.alignItems = "center";
+      info.innerHTML = `<a href="artist-page.html?id=${artistData.id}"><img src=${artistData.picture_small} class="rounded-circle me-2" width="60" height="60"></a>
+        <p class="text-light"><span class="text-light fw-bold">Hai messo Mi piace a 11 brani</span><br>di <a href="artist-page.html?id=${artistData.id}">${artistData.name}</a></p>`;
+      favoriteSongs.appendChild(info);
+    });
 }
 
-function displayArtistAlbum(artistData){
+function displayArtistAlbum(artistData) {
   fetch(endpoint + "/top?limit=50")
-    .then((response) => response.json())
-    .then((artistAlbums) => {
-  const artistAlbumList = document.getElementById("artistAlbumList")
-  artistAlbumList.innerHTML = `<div class="col-12 d-flex justify-content-between">
+    .then(response => response.json())
+    .then(artistAlbums => {
+      const artistAlbumList = document.getElementById("artistAlbumList");
+      artistAlbumList.innerHTML = `<div class="col-12 d-flex justify-content-between">
   <h3 class="text-white">Altro di ${artistData.name}</h3>
   <a href="">
     <p class="text-secondary fw-semibold">Vedi discografia</p>
   </a>
-  </div>`
+  </div>`;
 
-  artistAlbums.data.forEach(album => {
-    artistAlbumList.innerHTML += `<div class="col-3">
+      artistAlbums.data.forEach(album => {
+        artistAlbumList.innerHTML += `<div class="col-3">
     <a href="album-page.html?id=${album.album.id}"><div class="card bg-dark p-2" style="height:340px;">
       <img src="${album.album.cover_big}" class="card-img-top" alt="..."/>
       <div class="card-body">
@@ -119,7 +120,7 @@ function displayArtistAlbum(artistData){
       </div>
     </div>
     </a>
-  </div>`
-  })
-    })
+  </div>`;
+      });
+    });
 }
